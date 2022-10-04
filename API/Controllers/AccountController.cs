d6e0bf38a7abce9c371276637d843ad550771b8e
Taraf.Alonazi <Tarafnawaf77@gmail.com>
2022-10-04 08:51:37 +0300

    end of section 4

---
+++ API/Controllers/AccountController.cs
@@ -0,0 +1,89 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using API.Data;
+using API.DTOs;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Cryptography;
+using System.Text;
+using API.Interfaces;
+
+namespace API.Controllers
+{
+    public class AccountController : BaseAPIController
+    {
+        private readonly DataContext _context;
+        private readonly ITokenService _tokenService;
+
+        public AccountController(DataContext context, ITokenService tokenService)
+        {
+            _context = context;
+            _tokenService = tokenService;
+        }
+
+        [HttpPost("register")]
+        public async Task<ActionResult<UserDto>> Register(RegisterDto resisterDto)
+        {
+
+            if (await UserExist(resisterDto.Username)) return BadRequest("Username is taken");
+
+            using var hmac = new HMACSHA512();
+
+            var user = new AppUser
+            {
+
+                UserName = resisterDto.Username.ToLower(),
+                PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(resisterDto.Password)),
+                PasswordSalt = hmac.Key
+            };
+
+            _context.Users.Add(user);
+
+            await _context.SaveChangesAsync();
+
+            return new UserDto
+            {
+                Username = user.UserName,
+                Token = _tokenService.CreateToken(user)
+            };
+        }
+
+        [HttpPost("login")]
+        public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
+        {
+
+            var user = await _context.Users
+            .SingleOrDefaultAsync(x => x.UserName == loginDto.Username.ToLower());
+
+            if (user == null) return Unauthorized("Invalid username");
+
+            using var hmac = new HMACSHA512(user.PasswordSalt);
+
+            var computehash = hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDto.Password));
+
+            for (int i = 0; i < computehash.Length; i++)
+            {
+                if (computehash[i] != user.PasswordHash[i]) return Unauthorized("Invalid password");
+
+            }
+
+            return new UserDto
+            {
+                Username = user.UserName,
+                Token = _tokenService.CreateToken(user)
+            };
+        }
+
+
+
+        private async Task<bool> UserExist(string Username)
+        {
+
+            return await _context.Users.AnyAsync(x => x.UserName == Username.ToLower());
+        }
+    }
+}
---
+++ API/Services/TokenService.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using API.Entities;
+using API.Interfaces;
+using Microsoft.IdentityModel.Tokens;
+
+namespace API.Services
+{
+    public class TokenService : ITokenService
+    {
+        private readonly SymmetricSecurityKey _key;
+        public TokenService(IConfiguration config)
+        {
+            _key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config["TokenKey"]));
+
+        }
+
+        public string CreateToken(AppUser user)
+        {
+            var calims = new List<Claim>{
+
+                new Claim(JwtRegisteredClaimNames.NameId, user.UserName)
+
+            };
+
+            var cards = new SigningCredentials(_key, SecurityAlgorithms.HmacSha512Signature);
+            var TokenDescriptor = new SecurityTokenDescriptor{
+                Subject = new ClaimsIdentity(calims),
+                Expires = DateTime.Now.AddDays(7),
+                SigningCredentials = cards
+            };
+
+            var TokenHandler = new JwtSecurityTokenHandler();
+            var token = TokenHandler.CreateToken(TokenDescriptor);
+
+            return TokenHandler.WriteToken(token);
+        }
+
+        
+    }
+}
